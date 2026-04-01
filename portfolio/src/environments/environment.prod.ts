d7624756e074afe5dbeb733b1ac37a1cdbc60516
Lucas Alves Martins <lucasalvesmartins10@gmail.com>
2026-04-01 16:48:53 -0300

    Preparando para primeiro deploy

--- portfolio/src/environments/environment.prod.ts
+++ portfolio/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://lucas-programador.onrender.com',
 
   links: {
     front: 'https://github.com/Lukealveshow/front-app',
