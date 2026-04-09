594b1bd48fcad8d42e734c2a4a97c8ea7b21ecd2
Lucas Alves Martins <lucasalvesmartins10@gmail.com>
2026-04-09 15:05:23 -0300

    Preparando para deploy em produção

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { BlogComponent } from './pages/blog/blog';
 import { ArtigoComponent } from './pages/artigo/artigo';
+import { AboutComponent } from './pages/about/about';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
   {path: 'artigo/:slug', component: ArtigoComponent},
+  {path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '' }
 ];
